f4952c91c6d47a10306104b9612d4195b241ba74
Jake Burton <jburto94@yahoo.com>
2018-12-05 12:17:27 -0500

    Removed jQuery from extension and converted code to pure JavaScript

--- generate.js
+++ generate.js
@@ -1,33 +1,43 @@
-// Automatically clicks the load-more button, allowing it scrap the full list.
-$(document).ready(
-  setInterval(function(){
-    $('.load-more').click();
-    // If all content is loaded, then button disappears and the interval is stopped
-    if($('.load-more').length < 1) {
-      clearInterval();
+// Automatically loads all of user's watchlist to their screen
+(function() {
+  var click = setInterval(checkBtn, 300);
+  function checkBtn() {
+    let loadMore = document.querySelector('.load-more');
+    if(loadMore == null) {
+      clearInterval(click);
+    } else {
+      // clicks the load more button on the screen to load titles
+      loadMore.click();
     }
-  }, 300)
-)
+  }
+})();
 
-// Appends the Randomizer link on the right side of the page
-$(document).ready(function() {
-  setTimeout(
-    $(".ab_ninja:first").append("<p><a class='generate-random-movie' href='#'>Generate random title</a></p>")
-  , 8000)
-})
+ var aside = document.querySelector('.ab_ninja');
+// After 8 seconds, the link is added to the page
+// 8 seconds, so that the list has loaded properly
+setTimeout(
+  aside.insertAdjacentHTML(
+    'beforeend', 
+    '<p><a href="#" class="generate-btn">Generate random title</a></p>'
+  ), 
+8000);
 
-$(document).ready(function() {
-  setTimeout(
-    $('.generate-random-movie').click(function() {
-      var listed = $('.lister-item');
-      // When the link is clicked, it removes all titles from display;
-      $(listed).addClass('inactiveTitle');
-      // After setting them, it sets a single, random entry to become visible.
-      $(listed).removeClass('activatedTitle')
-        .eq([Math.floor(Math.random() * listed.length)])
-        .addClass('activatedTitle');
-    }), 8000)
-});
+const generateRandom = () => {
+  let listed = document.querySelectorAll('.lister-item');
+  // Adds inactive class to all elements, hiding them
+  for(let i = 0; i < listed.length; i++) {
+    listed[i].classList.add('inactiveTitle');
+    // Previously activated title has class removed to hide it
+    if(listed[i].classList.contains('activatedTitle')) {
+      listed[i].classList.remove('activatedTitle');
+    }
+  }
+  // Chooses random title to be activated
+  listed[Math.floor(Math.random() * listed.length)].classList.add('activatedTitle');
+}
+
+var generate = document.querySelector('.generate-btn');
+setTimeout(generate.addEventListener('click', generateRandom), 8000);
 
 // SetTimeouts are to let the user's page load, but the extension works best when
 // user manually scrolls through list once to let items load.
